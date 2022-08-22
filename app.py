2b8ed796e87c1ee75e697b78c869cb4f38536e2f
Sumit Luitel <sumitluitel@Sumits-MacBook-Air.local>
2022-08-22 09:20:42 +0545

    Flask API app

---
+++ app.py
@@ -0,0 +1,76 @@
+# Create virtual environment
+# $ python3 -m pip install flask
+# $ python3 -m pip install requests
+# Get api key from https://the-one-api.dev
+
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__)
+
+app.config.from_pyfile('config.py')
+
+API_KEY = app.config['API_KEY']
+
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    # Get books from api : https://the-one-api.dev/v2/book
+    response = requests.get("https://the-one-api.dev/v2/book")
+
+    extracted_data = response.json()
+    print(extracted_data)
+    # Convert response object to dict 
+    books = extracted_data['docs']
+    print(books)
+    # return jsonify(books)
+    return render_template('table.html', type="Books", data=books)
+
+
+@app.route('/movies', methods=["GET"])
+def get_movies():
+    # Set Authentication Header
+
+    headers = {"Authorization": f'Bearer {API_KEY}'}
+
+    # Get Movies from api: https://the-one-api.dev/v2/movie
+    response = requests.get("https://the-one-api.dev/v2/movie",
+                            headers=headers)
+
+    extracted_data = response.json()
+    movies = extracted_data["docs"]
+    print(movies)
+    return render_template('table.html', type="Movies", data=movies)
+
+
+@app.route('/characters', methods=["GET"])
+def get_characters():
+
+    # Set Authentication Header
+
+    headers = {"Authorization": f'Bearer {API_KEY}'}
+
+    # Get Characters from api: https://the-one-api.dev/v2/character
+
+    response = requests.get("https://the-one-api.dev/v2/character",
+                            headers=headers)
+
+    extracted_data = response.json()
+    characters = extracted_data["docs"]
+
+    return render_template('table.html', type='Characters', data=characters)
+
+@app.route('/players', methods=["GET"])
+def get_players():
+
+    # Get Characters from api: https://the-one-api.dev/v2/character
+
+    response = requests.get("http://localhost:5000")
+
+    extracted_data = response.json()
+    players = extracted_data
+    print(players)
+    return render_template('table.html', type='Players', data=players)
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
---
+++ config.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'Software_Fellowship_Day_4'
+DEBUG = True
+ENV = 'development'
+API_KEY = "LcbUD4gvN_aJsaf6U1ON"
